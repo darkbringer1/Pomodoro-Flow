0eb20c5ffd31fbf87d883d432b4c92752757cb41
Doğukaan Kılıçarslan <40922278+darkbringer1@users.noreply.github.com>
2021-08-23 13:11:44 +0300

    Initial Commit

---
+++ Pomodoro Flow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pomodoro Flow
+//
+//  Created by DarkBringer on 23.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pomodoro Flow/Pomodoro_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_FlowApp.swift
+//  Pomodoro Flow
+//
+//  Created by DarkBringer on 23.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
